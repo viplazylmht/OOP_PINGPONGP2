e76a7fda1b2c7aea42e7455e19c89e8240e31070
BHD233 <32787292+BHD233@users.noreply.github.com>
2019-12-02 10:44:16 +0700

    Create Obstacle class and change ball from struct to class (#1)
    New:
    -Ball now is a class
    -Add obstacle class
    To do:
    -Add method to ball
    -Create Bonus, Food, Wall

---
+++ Obstacle.cpp
@@ -0,0 +1,35 @@
+#include "Obstacle.h"
+
+Obstacle::Obstacle()
+{
+}
+
+Obstacle::Obstacle(char text, Point point, int color, bool isVisible)
+{
+}
+
+Obstacle::~Obstacle()
+{
+}
+
+void Obstacle::Draw()
+{
+}
+
+void Obstacle::Randome()
+{
+}
+
+int Obstacle::isNearBall(Ball ball)
+{
+	return 0;
+}
+
+bool Obstacle::isCollide(Ball ball)
+{
+	return false;
+}
+
+void Obstacle::CollidePocess(int& point, Ball& ball)
+{
+}
---
+++ Point.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <iostream>
+
+using namespace std;
+
+struct Point
+{
+    int x = 0;
+    int y = 0;
+};
